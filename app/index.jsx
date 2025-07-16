6963f9af169221755cef617281d0e1c58dc9b7d3
Deann7 <deandronas04@gmail.com>
2025-07-16 12:08:30 +0700

    first

---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home</Text>
+      <Text ></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
